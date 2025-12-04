ab99e209715d1fd5704ea75219cd439fb55908a9
Xico26 <xicomaia2610@gmail.com>
2025-12-04 22:34:12 +0000

    day 3 solution

--- src/main/java/Day03.java
+++ src/main/java/Day03.java
@@ -1,14 +1,15 @@
 import common.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day03 {
     List<String> blocks;
-    int joltageSum;
+    Long joltageSum;
 
     public Day03(String inputFileName) {
         this.blocks = Utils.parseInput(inputFileName);
-        joltageSum = 0;
+        joltageSum = 0L;
     }
 
     public void run() {
@@ -23,7 +24,9 @@ public class Day03 {
     }
 
     public void processBlock(String block) {
+        /* Part 1
         int maxJolt = 0;
+
 
         for (int i = 0; i < block.length(); i++) {
             char c = block.charAt(i);
@@ -38,8 +41,43 @@ public class Day03 {
                 }
             }
         }
+         */
 
-        System.out.println(maxJolt);
-        joltageSum += maxJolt;
+        int numDigits = 12; // swap to 2 for part 1
+        List<Integer> batteryJoltages = new ArrayList<>();
+        int lastIndex = -1;
+        for (int i = 0; i < numDigits; i++) {
+            int windowStart = lastIndex + 1;
+            int windowEnd = block.length() - (numDigits - i - 1);
+
+            System.out.println(i + " " + windowStart + " " + windowEnd);
+            if (windowStart < windowEnd) {
+                String substr =  block.substring(windowStart, windowEnd);
+                System.out.println("Now evaluating " + substr);
+
+                int max = 0;
+                int bestIndex = -1;
+                for (int j = 0; j < substr.length(); j++) {
+                    int elem = Integer.parseInt(substr.charAt(j) + "");
+                    if (elem > max) {
+                        max = elem;
+                        bestIndex = j;
+                    }
+                }
+                lastIndex = bestIndex + windowStart;
+
+                batteryJoltages.add(max);
+            } else {
+                break;
+            }
+        }
+
+        Long jolt = 0L;
+        for (Integer batteryJoltage: batteryJoltages) {
+            jolt = jolt * 10 +  batteryJoltage;
+        }
+
+        System.out.println(jolt);
+        joltageSum += jolt;
     }
 }
