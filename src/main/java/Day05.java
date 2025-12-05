1b1ca58b2522f60656c7d841e18e3ea5f5a3aadf
Xico26 <xicomaia2610@gmail.com>
2025-12-05 22:53:11 +0000

    day 5 part 2 (not finished)

--- src/main/java/Day05.java
+++ src/main/java/Day05.java
@@ -8,12 +8,14 @@ public class Day05 {
     private List<String> validIds;
     private List<String> ingredientIds;
     private List<Long> freshIngredients;
+    long numberOfValidIngredients;
 
     public Day05 (String inputFileName) {
         List<String> inputsList = Utils.parseInput(inputFileName);
         this.validIds = new ArrayList<>();
         this.ingredientIds = new ArrayList<>();
         this.freshIngredients = new ArrayList<>();
+        this.numberOfValidIngredients = 0;
 
         boolean hasFinishedIds = false;
         for (String l: inputsList) {
@@ -46,6 +48,14 @@ public class Day05 {
     public void run () {
         // printLists();
 
+        // PART 1
+        // validateIngredients();
+
+        // PART 2
+        checkAllRanges();
+    }
+
+    public void validateIngredients () {
         for (String l: ingredientIds) {
             long id = Long.parseLong(l);
             boolean isFresh = checkIngredient(id);
@@ -62,7 +72,6 @@ public class Day05 {
 
         int count = this.freshIngredients.size();
         System.out.println("Number of fresh ingredients: " + count);
-
     }
 
     public boolean checkIngredient (long id) {
@@ -72,10 +81,10 @@ public class Day05 {
             String start = split[0];
             String end = split[1];
 
-            long startInt =  Long.parseLong(start);
-            long endInt = Long.parseLong(end);
+            long startVal =  Long.parseLong(start);
+            long endVal = Long.parseLong(end);
 
-            if (id >= startInt && id <= endInt) {
+            if (id >= startVal && id <= endVal) {
                 return true;
             }
         }
@@ -83,4 +92,79 @@ public class Day05 {
         return false;
     }
 
+    public List<String> mergeRanges () {
+        List<String> newRanges = new ArrayList<>(this.validIds);
+        boolean hasChanged = true;
+        int i = 0;
+        while (hasChanged && i < newRanges.size()) {
+            hasChanged = mergeRange(newRanges.get(i), newRanges);
+            if (!hasChanged) {
+                hasChanged = true;
+                i++;
+            }
+        }
+
+        return newRanges;
+    }
+
+    private boolean mergeRange (String line, List<String> newRanges) {
+        String[] split = line.split("-");
+
+        String start = split[0];
+        String end = split[1];
+
+        long startVal = Long.parseLong(start);
+        long endVal = Long.parseLong(end);
+
+        int i = 0;
+        while (i < newRanges.size()) {
+            String range = newRanges.get(i);
+            if (range.equals(line)) {
+                i++;
+                continue;
+            }
+            // System.out.println("Can I merge " + line + " with " + range);
+            String[] split2 = range.split("-");
+
+            String start2 = split2[0];
+            String end2 = split2[1];
+
+            long startVal2 = Long.parseLong(start2);
+            long endVal2 = Long.parseLong(end2);
+
+            // System.out.println("start " + start + "; end " + end + "; start2 " + start2 + "; end2 " + end2);
+
+            if (startVal2 <= endVal + 1 && endVal2 >= startVal) {
+                // System.out.println("Merging " + line + " with " + range);
+                StringBuilder sb = new StringBuilder();
+                sb.append(Math.min(startVal, startVal2));
+                sb.append("-");
+                sb.append(Math.max(endVal, endVal2));
+                newRanges.remove(line);
+                newRanges.remove(range);
+                newRanges.add(sb.toString());
+                return true;
+            }
+
+            i++;
+        }
+
+        return false;
+    }
+
+    public void checkAllRanges () {
+        List<String> newRanges = mergeRanges();
+        System.out.println("new ranges");
+        for (String l: newRanges) {
+            // getAllValidIds(l);
+            System.out.println(l);
+            String[] split = l.split("-");
+
+            String start = split[0];
+            String end = split[1];
+            this.numberOfValidIngredients += ((Long.parseLong(end) - Long.parseLong(start)) + 1);
+        }
+
+        System.out.println("Number of valid ingredients: " + this.numberOfValidIngredients);
+    }
 }
